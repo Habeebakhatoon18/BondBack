6fd91efcd4d08951630b37b09b47f509997730c6
Habeeba Khatoon <Khabeeba18@gmail.com>
2024-12-13 19:43:43 +0530

    probably last commit

--- script.js
+++ script.js
@@ -22,9 +22,13 @@ var navLinks=document.getElementById("navLinks");
             /*navLinks.style.right="-200px";*/
             navLinks.style.display="none";
         }
+
 const uibtn = document.querySelector('.btn-hover-ui');
 const pybtn = document.querySelector('.btn-hover-py');
 const pdbtn = document.querySelector('.btn-hover-pd');
+const google = document.querySelector('.google');
+const uber = document.querySelector('.uber');
+const amazon = document.querySelector('.amazon');
 
 const linkToui = 'https://trainings.internshala.com/ui-ux-placement-guarantee-course/?utm_source=Google-Search&utm_campaign=INT-Search-Exact-STT-Non-Brand-UI-UX-PGC-Aug24&utm_adgroup=Course&utm_locationinterest=&utm_searchkeyword=ui%20and%20ux%20course&utm_keywordid=kwd-823735660294&gad_source=1&gclid=Cj0KCQiA0--6BhCBARIsADYqyL8J5fKShRMW5Hw3HMfz0HN4HaEn14OryAlBd4vUteyKftxa0OT-cnkaAlWPEALw_wcB';
 const linkTopy = 'https://www.udemy.com/topic/python/?utm_source=adwords&utm_medium=udemyads&utm_campaign=Brand-Topic_la.EN_cc.India&campaigntype=Search&portfolio=BrandTopic&language=EN&product=Course&test=&audience=Keyword&topic=&priority=&utm_content=deal4584&utm_term=_._ag_139989572110_._ad_719478367800_._kw_udemy%20python_._de_m_._dm__._pl__._ti_kwd-317120314556_._li_9062152_._pd__._&matchtype=b&gad_source=1&gclid=Cj0KCQiA0--6BhCBARIsADYqyL-P4lIg2CW8To_zZdnlL3JXCuG9Vrqq17-lobXm1AN_o0kxXGP5vBwaAiCFEALw_wcB';
@@ -45,4 +49,15 @@ pdbtn.addEventListener('click', function() {
     pdbtn.focus();
     window.location.href = linkTopd;
 });
-
+google.addEventListener('click', function() {
+    google.focus();
+    window.location.href = "https://about.google/brand-resource-center/guidance/sponsorships/";
+});
+amazon.addEventListener('click', function() {
+    amazon.focus();
+    window.location.href ="https://aws.amazon.com/partners/marketing/sponsorships/" ;
+});
+uber.addEventListener('click', function() {
+    uber.focus();
+    window.location.href ="https://flows.beamery.com/uber/apac-uber-she---2025-s_29svpsn" ;
+});
